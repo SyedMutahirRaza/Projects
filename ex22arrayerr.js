b9d407c2b7f06e823ea14237d035839aa5d72c25
SyedMutahirRaza <smraza_999@hotmail.com>
2024-04-21 05:53:11 +0500

    Initial Commit

--- ex22arrayerr.js
+++ ex22arrayerr.js
@@ -1,2 +1,10 @@
-var array1 = [1, 2, 3, 4, 5];
-var indexerr = array1[3];
+var myArray = [1, 2, 3, 4, 5];
+// Intentional Error: Trying to access an element at an index that doesn't exist
+var indexError = myArray[1]; // This will produce an index error
+// Correcting the error by checking if the index is within the bounds of the array
+if (indexError !== undefined) {
+    console.log("Element at index :", indexError);
+}
+else {
+    console.log("Index is out of bounds.");
+}
